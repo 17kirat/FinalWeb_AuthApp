18f9c6fd3b94547e64770358aefc79bd5c4c6eac
Jaskirat KAUR <19kirat@gmail.com>
2022-04-09 22:08:19 -0400

    A new identity item is added with login and Register pages.

---
+++ Areas/Identity/Data/AuthDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinalWeb_AuthApp.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalWeb_AuthApp.Data
+{
+    public class AuthDbContext : IdentityDbContext<FinalWeb_AuthAppUser>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
